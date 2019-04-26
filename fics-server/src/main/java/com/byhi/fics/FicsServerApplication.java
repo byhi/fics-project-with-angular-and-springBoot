10e90c7e9fe2a5968d2ff5c8d37deb68945955d0
Bihari Zoltán <byhisoft@gmail.com>
2019-04-26 12:43:56 +0200

    add spring project

---
+++ fics-server/src/main/java/com/byhi/fics/FicsServerApplication.java
@@ -0,0 +1,13 @@
+package com.byhi.fics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicsServerApplication.class, args);
+	}
+
+}
