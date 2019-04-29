8e986c639e218da79c18876f330d4b3639f26705
Bihari Zoltán <byhisoft@gmail.com>
2019-04-29 10:34:31 +0200

    Refaktoráltam a kontrollereket, @CrossOrigin-t raktam rendszerbe.

--- fics-server/src/main/java/com/byhi/fics/controller/ModulController.java
+++ fics-server/src/main/java/com/byhi/fics/controller/ModulController.java
@@ -16,9 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.byhi.fics.domain.Modul;
 import com.byhi.fics.service.ModulServiceImpl;
 import com.byhi.fics.service.RendszerServiceImpl;
-
-@RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@RestController
 public class ModulController {
 
 	private ModulServiceImpl modulServiceImpl;
@@ -34,31 +33,29 @@ public class ModulController {
 	public void getRendszerServiceImpl(RendszerServiceImpl rendszerService) {
 		this.rendszerService = rendszerService;
 	}
-
-	@GetMapping("/modul/{rendszerId}")
 	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/modul/{rendszerId}")
 	public Collection<Modul> getModuls(@PathVariable String rendszerId) {
 		System.out.println("getModuls " + rendszerId);
 		return rendszerService.findById(new Long(Integer.valueOf(rendszerId))).getModuls().stream()
 				.collect(Collectors.toList());
 	}
-
-	@GetMapping("/modul/all")
 	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/modul/all")
 	public Collection<Modul> getAllModul() {
 		return modulServiceImpl.findAll().stream().collect(Collectors.toList());
 	}
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/modul/add")
 	void addModul(@RequestBody Modul rendszer) {
 		modulServiceImpl.addModul(rendszer);
 	}
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	@DeleteMapping("/modul/{rendszerId}")
 	void deleteModul(@PathVariable String rendszerId) {
 		modulServiceImpl.deleteModul(new Long(Integer.valueOf(rendszerId)));
 	}
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/modul/{id}}")
 	void updateModul(@RequestBody Modul modul) {
 		modulServiceImpl.updateModul(modul);
--- fics-server/src/main/java/com/byhi/fics/controller/RendszerController.java
+++ fics-server/src/main/java/com/byhi/fics/controller/RendszerController.java
@@ -13,9 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.byhi.fics.domain.Rendszer;
 import com.byhi.fics.service.RendszerServiceImpl;
-
-@RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@RestController
 public class RendszerController {
 
 	private RendszerServiceImpl rendszerService;
@@ -24,32 +23,32 @@ public class RendszerController {
 	public void getRendszerServiceImpl(RendszerServiceImpl rendszerService) {
 		this.rendszerService = rendszerService;
 	}
-
-	@GetMapping("/rendszer/all")
+	
 	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/rendszer/all")
 	public Collection<Rendszer> getAllRendszer() {
 
 		return rendszerService.findAll().stream().collect(Collectors.toList());
 	}
 
-	@GetMapping("/rendszer/{rendszerId}")
 	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/rendszer/{rendszerId}")
 	public Rendszer getRendszer(@PathVariable String rendszerId) {
 		return rendszerService.findById(new Long(Integer.valueOf(rendszerId)));
 	}
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/rendszer/add")
 	void addRendszer(@RequestBody Rendszer rendszer) {
 		rendszerService.addRendszer(rendszer);
 	}
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	@DeleteMapping("/rendszer/{rendszerId}")
 	void deleteRendszer(@PathVariable String rendszerId) {
 		rendszerService.deleteRendszer(new Long(Integer.valueOf(rendszerId)));
 	}
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PutMapping("/rendszer")
-	@CrossOrigin(origins = "http://localhost:4200/rendszer/")
 	void updateRendszer(@RequestBody Rendszer rendszer) {
 		rendszerService.updateRendszer(rendszer);
 	}
