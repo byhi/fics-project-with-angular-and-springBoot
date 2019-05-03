bd578b0a6b9ed52988a3b5fd8c36291482aeea47
Bihari Zoltán <byhisoft@gmail.com>
2019-05-03 19:04:00 +0200

    Servicek kiegészitve az implementálandó metódusokkal.

--- fics-server/src/main/java/com/byhi/fics/service/ModulService.java
+++ fics-server/src/main/java/com/byhi/fics/service/ModulService.java
@@ -1,5 +1,16 @@
 package com.byhi.fics.service;
 
+import com.byhi.fics.domain.Modul;
+import com.byhi.fics.domain.Rendszer;
+
 public interface ModulService {
+
+	Modul findById(Long long1);
+
+	void deleteModul(Long long1);
+
+	void updateModul(Modul modul, Rendszer findById);
+
+	void addModul(Modul modul, Rendszer findById);
 
 }
--- fics-server/src/main/java/com/byhi/fics/service/RendszerService.java
+++ fics-server/src/main/java/com/byhi/fics/service/RendszerService.java
@@ -8,4 +8,12 @@ import com.byhi.fics.domain.Rendszer;
 @Service
 public interface RendszerService {
 	Collection<Rendszer> findAll();
+
+	Rendszer findById(Long long1);
+
+	void addRendszer(Rendszer rendszer);
+
+	void deleteRendszer(Long long1);
+
+	void updateRendszer(Rendszer rendszer);
 }
